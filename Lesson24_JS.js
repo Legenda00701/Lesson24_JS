0db3f4fa2647c50afb3697e72e482a0fe1dc2905
Legenda00701 <deldla917@gmail.com>
2024-09-24 16:15:55 +0600

    -

---
+++ Lesson24_JS.js
@@ -0,0 +1,159 @@
+//1
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+
+//2
+
+// let sum = 0;
+
+// for (let i = 0; i <= 10; i++) {
+//     sum += i;
+// }
+// console.log(sum)
+
+
+//3
+
+// let factorial = 1;
+
+// for (let i = 1; i <= 5; i++) {
+//     factorial *= i;
+// }
+// console.log(factorial)
+
+
+//4
+
+// let str = "Hello, World!";
+
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+
+//5
+
+// let array = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+
+//6
+
+// let array = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+// }
+// console.log(sum);
+
+
+//7
+
+// let array = [1, 2, 3, 4, 5];
+// let max = array[0];
+
+// for (let i = 1; i < array.length; i++) {
+//     if (array[i] > max) {
+//         max = array[i];
+//     }
+// }
+// console.log(max);
+
+
+//8
+
+// let array = [1, 2, 3, 1, 2, 1];
+// let count = 0;
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 1) {
+//         count++;
+//     }
+// }
+// console.log(count);
+
+
+//9
+
+// let squares = [];
+
+// for (let i = 1; i <= 10; i++) {
+//     squares.push(i * i);
+// }
+// console.log(squares);
+
+
+//10
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+//11
+
+// let powersOfTwo = [];
+
+// for (let i = 0; i < 10; i++) {
+//     powersOfTwo.push(Math.pow(2, i));
+// }
+// console.log(powersOfTwo);
+
+
+//12
+
+// let array = [5, 9, 3, 7, 2, 8, 1, 6, 4];
+// let min = array[0];
+
+// for (let i = 1; i < array.length; i++) {
+//     if (array[i] < min) {
+//         min = array[i];
+//     }
+// }
+// console.log(min);
+
+
+//13
+
+// let str = "Hello, World!";
+// let reversed = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+// }
+// console.log(reversed);
+
+
+//14
+
+// let array = [10, 12, 31, 40, 15];
+// let target = 31;
+// let index = -1;
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === target) {
+//         index = i;
+//         break;
+//     }
+// }
+// console.log(index);
+
+
+//15
+
+// let array = [1, 1, 1, 1, 1];
+// let allEqual = true;
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] !== 1) {
+//         allEqual = false;
+//         break;
+//     }
+// }
+// console.log(allEqual);
